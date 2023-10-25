07d09fb8c4aff9a93b7aef786b070399a43a0736
JeanLopez07 <148912609+JeanLopez07@users.noreply.github.com>
2023-10-25 13:15:44 -0500

    pruebadd_time.php

--- pruebadd_time.php
+++ pruebadd_time.php
@@ -1,44 +1,36 @@
 <?php
-function add_time($inicio,$duracion,$inicio_dia = null) {
+function add_time($inicio, $duracion, $inicio_dia = null) {
 
-        list($hora_inicio,$minuto_inicio,$tarde) = explode(" ",$inicio);
-        list($duracion_hora,$duracion_minuto) = explode(":",$duracion);
-
-        $iniciar_minutos=$hora_inicio * 60 + $minuto_inico
-        $duracion_minutos=$duracion_hora * 60 + $duracion_minuto;
-
+        list($hora_inicio, $minuto_inicio,$tarde) = explode(" ", $inicio);
+        list($duracion_hora, $duracion_minuto) = explode(":", $duracion);
+    
+        $iniciar_minutos = $hora_inicio * 60 + $minuto_inicio;
+        $duracion_minutos = $duracion_hora * 60 + $duracion_minuto;
+    
         $minutos_totales = $iniciar_minutos + $duracion_minutos;
-
-        $hora_final = floor ($total_minutos/60)
+    
+        $hora_final = floor($minutos_totales / 60);
         $minuto_final = $minutos_totales % 60;
+    
+        $dias_despues = floor($minutos_totales / 1440);
+        $dias_despues_texto = "";
+    
+        if ($dias_despues == 1) {
+            $dias_despues_texto = " (día siguiente)";
+        } elseif ($dias_despues > 1) {
+            $dias_despues_texto = " ({$dias_despues} días después)";
+        }
+    
+        if ($inicio_dia !== null) {
+            $inicio_dia = ucfirst(strtolower($inicio_dia));
+            $result = "{$hora_final}:{$minuto_final} {$tarde}{$dias_despues_texto}, {$inicio_dia}";
+        } else {
+            $result = "{$hora_final}:{$minuto_final} {$tarde}{$dias_despues_texto}";
+        }
+    
+        return $result;
+    }
+    
+    echo add_time("3:00 PM", "3:10");
 
-        $dias_despues = floor($minutos_totales/1440);
-        $dias_despues_texto ="";
-
-        if ($dias_despues == 1){
-            $dias_despues_texto= "(dia siguiente)";
-
-}       elseif ($dias_despues > 1){
-                $dias_despues_texto=({dias_despues}) dias despues)";
-
-}
-
-
-
-
-        if ($iniciar_dia !== null)  {
-                $iniciar_dia= ucfirst (strtolower($iniciar_dia));
-                result "{$hora_final}: {$minuto_final}:$tarde$dias_despues_texto,$iniciar_dia";
-
-}               else  {
-
-
-                result "{$hora_final}: {$minuto_final}:$tarde$dias_despues_texto" ;
-
-
-}
-
-                result $result;
-}
-                echo add_time("3:00 PM","3:10");
 ?>
